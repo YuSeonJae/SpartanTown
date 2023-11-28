655db2956523278783c4b4635bac112ddb5c8583
YuSeonJae <985221@naver.com>
2023-11-28 23:03:26 +0900

    SpartanTownInit
    SpartanTown ver.1
    Move, Move Action(jump), Look Mouse

---
+++ Assets/Scripts/Entities/TopDownAimRotation.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownAimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownCharacerController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<TopDownCharacerController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller.OnLookEvent += OnAim;
+    }
+
+    public void OnAim(Vector2 newAimDirection)
+    {
+        RotateArm(newAimDirection);
+    }
+
+    private void RotateArm(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
